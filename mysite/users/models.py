612809957c44f7b39d4f9df3d061ce71a23d44b6
VivekPatil81400 <59966933+VivekPatil81400@users.noreply.github.com>
2022-02-11 19:28:40 +0100

    second commit

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
+import imp
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(default='profilepic.jpg', upload_to ='profile_pictures')
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
